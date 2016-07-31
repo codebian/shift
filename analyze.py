0ac1d25adaa201539c2011dff0694ac214a71a5d
jeeeberhardt <qksoneo@gmail.com>
2016-07-31 11:27:50 +0200

    first commit

---
+++ analyze.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+import argparse
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from Bio.SeqUtils import seq3
+from matplotlib import pyplot as plt
+
+__author__ = "Jérôme Eberhardt, Roland H Stote, and Annick Dejaegere"
+__copyright__ = "Copyright 2016, Jérôme Eberhardt"
+__credits__ = ["Jérôme Eberhardt", "Roland H Stote", "Annick Dejaegere"]
+
+__lience__ = "MIT"
+__maintainer__ = "Jérôme Eberhardt"
+__email__ = "qksoneo@gmail.com"
+
+def get_obs(obs_file):
+    return pd.read_csv(obs_file, delim_whitespace=True, usecols=[1,2,3,5], 
+                       names=['resid', 'resn', 'atom_type', 'obs'])
+
+def get_random_coil(rc_file):
+    rc = pd.read_csv(rc_file, usecols=[1,2,3], names=['resn', 'atom_type', 'rc'])
+    rc['resn'] = rc['resn'].apply(seq3).str.upper()
+    return rc
+
+def get_pred(hdf_file, atom_types):
+    columns = ['resid', 'resn', 'atom_type', 'pred', 'std']
+    df = pd.DataFrame(np.nan, index=[0], columns=columns)
+
+    with h5py.File(hdf_file, 'r') as f:
+
+        i = 0
+
+        for residue in f.iteritems():
+            resid = int(residue[0].split('_')[0])
+            resn = str(residue[0].split('_')[1])
+
+            for atom_type in atom_types:
+
+                try:
+                    nset = '%s_%s/%s' % (resid, resn, atom_type)
+                    dset = f[nset]
+
+                    df.loc[i] = [resid, resn, atom_type, np.nanmean(dset), np.nanstd(dset)]
+
+                    i += 1
+                except KeyError:
+                    pass
+
+    return df
+
+def rmsd(x, y):
+    diff = x - y
+    diff = diff.dropna()
+    return np.sqrt(np.sum(diff**2)/(float(diff.shape[0])))
+
+def extract_dssp_info(dssp_file, sectype='H', shift = 0):
+    
+    start_read = False
+    list_sec = []
+    i = 0
+    
+    with open(dssp_file) as f:
+        for line in f:
+            if '  #  RESIDUE' in line:
+                start_read = True
+            
+            if start_read and not "#" in line:
+                resnum = int(line[6:10].strip()) + shift
+                secstr = str(line[16])
+                
+                if sectype == secstr:
+                    i += 1
+                else:
+                    if i: list_sec.append((resnum-i, i))
+                    i = 0
+    
+    return list_sec
+
+def plot_dssp_info(ax, resid, min_data, max_data, dssp_file, baseline=None):
+    
+    range_data = max_data - min_data
+    
+    if baseline != None:
+        y_min = baseline - (range_data / 10.)
+    else:
+        y_min = min_data - (range_data / 10.)
+        
+    y_bar = y_min + (np.abs(min_data - y_min) / 2.)
+    y_box = y_bar + (np.abs(y_bar - y_min) / 2.)
+    y_len = 2. * (y_bar - y_box)
+    
+    ax.broken_barh(extract_dssp_info(dssp_file, 'H'), (y_box, y_len), facecolors='SteelBlue', zorder=3)
+    ax.broken_barh(extract_dssp_info(dssp_file, 'E'), (y_box, y_len), facecolors='Gold', zorder=3)
+    ax.plot(resid, len(resid)*[y_bar], color='black', zorder=1, lw=0.5)
+
+def plot_shift(cs, dssp_file):
+
+    color = 'dodgerblue'
+    atom_types = cs['atom_type'].unique()
+
+    xmin = cs['resid'].min()
+    xmax = cs['resid'].max()
+
+    label = {'CA': 'C_{\\alpha}', 'CB': 'C_{\\beta}'}
+
+    for atom_type in atom_types:
+
+        if label.has_key(atom_type):
+            ylabel = label[atom_type]
+        else:
+            ylabel = atom_type
+
+        fig, ax = plt.subplots(figsize=(12, 4))
+
+        tmp = cs.loc[cs['atom_type'] == atom_type]
+
+        ax.axhline(linewidth=1, color='Gainsboro')
+        ax.plot(tmp['resid'], tmp['dpred'], linestyle='-', marker='.', color=color)
+        ax.plot(tmp['resid'], tmp['dobs'], linestyle='-', marker='.', color='black')
+
+        std_neg = tmp['dpred'] - tmp['std']
+        std_pos = tmp['dpred'] + tmp['std']
+
+        ax.fill_between(tmp['resid'], std_neg, std_pos, color=color, alpha=0.4)
+
+        ymax = np.max(np.abs(tmp['dobs'])) * 1.5
+        ymin = -ymax
+
+        plot_dssp_info(ax, tmp['resid'], ymin / 1.5, ymax, dssp_file)
+
+        ax.set_xlabel("Residue", fontsize=20)
+        ax.set_ylabel(r"$\Delta\delta %s$ (ppm)" % ylabel, fontsize=20)
+        ax.set_xlim(xmin - 1, xmax + 1)
+        ax.set_ylim(ymin, ymax)
+
+        plt.savefig("shift_%s.png" % atom_type, dpi=300, format='png', bbox_inches='tight')
+
+        plt.close(fig)
+
+def plot_distribution(cs, hdf_file):
+
+    color = 'dodgerblue'
+    atom_types = cs['atom_type'].unique()
+
+    with h5py.File(hdf_file, 'r') as f:
+
+        for atom_type in atom_types:
+
+            out_dir = 'shift_distribution/%s' % atom_type
+            os.makedirs(out_dir)
+
+            cols = 10
+            num = cs['atom_type'].value_counts()[atom_type]
+            rows = int(num / cols) + (num % cols > 0)
+
+            figB, axB = plt.subplots(rows, cols, figsize=(35, 45), sharex=True, sharey=True)
+
+            icol, irow = 0, 0
+
+            xmax = 0
+
+            # We want the global xmax !
+            for residue in f.iteritems():
+                resid = int(residue[0].split('_')[0])
+                resn = str(residue[0].split('_')[1])
+
+                try:
+                    nset = '%s_%s/%s' % (resid, resn, atom_type)
+                    dset = f[nset]
+
+                    row = cs[(cs['resid'] == resid) & (cs['resn'] == resn) & (cs['atom_type'] == atom_type)]
+
+                    x = dset - row['rc'].values[0]
+                    xmax = np.nanmax([xmax, np.nanmax(np.abs(x)), np.abs(row['dobs']).values[0]])
+
+                except KeyError:
+                    pass
+
+            xmax *= 1.5
+
+            for residue in f.iteritems():
+                resid = int(residue[0].split('_')[0])
+                resn = str(residue[0].split('_')[1])
+
+                try:
+                    nset = '%s_%s/%s' % (resid, resn, atom_type)
+                    dset = f[nset]
+
+                    y, x = np.histogram(dset, bins=50)
+
+                    row = cs[(cs['resid'] == resid) & (cs['resn'] == resn) & (cs['atom_type'] == atom_type)]
+
+                    # Get Delta_delta CS
+                    x = x[:-1] - row['rc'].values[0]
+                    # Get density (something is wrong with numpy density ...)
+                    y = y / np.float(np.sum(y))
+                    
+                    # For the small picture !
+                    fig, ax = plt.subplots(figsize=(6, 2))
+                    ax.plot(x, y, linestyle='-', color=color)
+                    ax.plot(row['dobs'], 0, marker='^', markersize=30, color='black')
+                    ax.plot(row['dpred'], 0, marker='^', markersize=25, color=color)
+
+                    ax.set_ylim(0, 0.20)
+                    ax.set_xlim(-xmax, xmax)
+                    ax.set_ylabel('Population', fontsize=20)
+                    ax.set_xlabel(r'$\Delta\delta$ (ppm)', fontsize=20)
+
+                    fig_name = "%s/%s_%s.png" % (out_dir, resid, resn)
+                    fig.savefig(fig_name, dpi=300, format='png', bbox_inches='tight')
+
+                    plt.close(fig)
+
+                    # And now the BIG picture !
+                    axB[irow, icol].plot(x, y, linestyle='-', color=color)
+                    axB[irow, icol].plot(row['dobs'], 0, marker='^', markersize=30, color='black')
+                    axB[irow, icol].plot(row['dpred'], 0, marker='^', markersize=25, color=color)
+
+                    axB[irow, icol].set_ylim(0, 0.20)
+                    axB[irow, icol].set_xlim(-xmax, xmax)
+                    axB[irow, icol].set_title("%s - %s" % (resid, resn))
+
+                    if icol == 0:
+                        axB[irow, icol].set_ylabel('Population', fontsize=15)
+
+                    if irow == rows - 1:
+                        axB[irow, icol].set_xlabel(r'$\Delta\delta$ (ppm)', fontsize=15)
+
+                    icol += 1
+
+                    if icol == cols:
+                        icol = 0
+                        irow += 1
+
+                except KeyError:
+                    pass
+
+            fig_name = "%s/distribution_%s_all.png" % (out_dir, atom_type)
+            figB.savefig(fig_name, dpi=300, format='png', bbox_inches='tight')
+
+            plt.close(figB)
+
+def main():
+    parser = argparse.ArgumentParser(description='Analyze NMR chemical shift')
+    parser.add_argument('-c', "--obs", dest='obsfile', required = True, \
+                        action="store", type=str, \
+                        help = "bmrb file with experimental chemical shift")
+    parser.add_argument("-h5", dest='h5file', required = True, \
+                        action = "store", type=str, \
+                        help = "provide a hdf5 file with all nmr data")
+    parser.add_argument("-d", "--dssp", dest='dsspfile', \
+                        action = "store", type=str, default = None, \
+                        help = "provide a dssp file for secondary structure")
+    parser.add_argument("--distribution", dest='distribution', \
+                        action = "store_true", default = False, \
+                        help = "if we want to plot all the distribution")
+
+    options = parser.parse_args()
+    
+    obs_file = options.obsfile
+    hdf_file = options.h5file
+    dssp_file = options.dsspfile
+    distribution = options.distribution
+
+    try:
+        shiftx2_dir = os.environ['SHIFTX2_DIR']
+        rc_file = shiftx2_dir + '/lib/RandomCoil.csv'
+    except KeyError:
+        print 'Error: The environ variable SHIFTX2_DIR is not set !'
+        sys.exit(1)
+
+    # Get obs and random coil
+    cs_obs = get_obs(obs_file)
+    # Get mean and std pred
+    cs_pred = get_pred(hdf_file, cs_obs['atom_type'].unique())
+    # Get Random coil values
+    rc = get_random_coil(rc_file)
+
+    # Merge all the data together
+    cs = pd.merge(cs_obs, cs_pred, how='right', on=['resid', 'resn', 'atom_type'])
+    cs = cs.sort_values(by='resid').reset_index(drop=True)
+    cs = pd.merge(cs, rc, how='left', on=['resn', 'atom_type'])
+
+    # Get Delta_delta chemical shift
+    cs['dobs'] = cs['obs'] - cs['rc']
+    cs['dpred'] = cs['pred'] - cs['rc']
+
+    # Compute RMSD between obs and pred for each atom type
+    for atom_type in cs['atom_type'].unique():
+        tmp = cs.loc[cs['atom_type'] == atom_type]
+        print '%3s %5.3f' % (atom_type,  rmsd(tmp['dobs'], tmp['dpred']))
+
+    # Save and plot data
+    cs.to_csv('shift_resume.csv', index=False, na_rep='NaN')
+    plot_shift(cs, dssp_file)
+
+    if distribution:
+        plot_distribution(cs, hdf_file)
+
+if __name__=="__main__":
+    main()
+    sys.exit(0)
---
+++ shift.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Extract all the chemical shift from MD trajectory using SHIFTX+ from SHIFTX2 """
+
+import os
+import re
+import sys
+import glob
+import shlex
+import shutil
+import fnmatch
+import argparse
+import subprocess
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from mpi4py import MPI
+from MDAnalysis import Universe
+
+__author__ = "Jérôme Eberhardt, Roland H Stote, and Annick Dejaegere"
+__copyright__ = "Copyright 2016, Jérôme Eberhardt"
+__credits__ = ["Jérôme Eberhardt", "Roland H Stote", "Annick Dejaegere"]
+
+__lience__ = "MIT"
+__maintainer__ = "Jérôme Eberhardt"
+__email__ = "qksoneo@gmail.com"
+
+#pd.set_option('display.max_rows', None)
+
+class Shiftx:
+    def __init__(self, pdb_dir=None, top_file=None, dcd_file=None):
+
+        # Define MPI environnment
+        self.comm = MPI.COMM_WORLD
+        self.rank = self.comm.Get_rank()
+        self.size = self.comm.Get_size()
+
+        if self.rank == 0:
+            if pdb_dir and (top_file or dcd_file):
+                print 'Warning: cs will be extracted from pdb and not from dcd'
+            elif not pdb_dir and not (top_file and dcd_file):
+                print 'Error: pdb file or (top and dcd files) are required'
+                self.comm.Abort()
+
+        self.pdb_dir = pdb_dir
+        self.top_file = top_file
+        self.dcd_file = dcd_file
+
+        # And check if SHIFTX2_DIR environnment variable is set
+        if self.rank == 0:
+            try:
+                self.shiftx2_dir = os.environ['SHIFTX2_DIR']
+            except KeyError:
+                print 'Error: The environ variable SHIFTX2_DIR is not set!'
+                self.comm.Abort()
+        else:
+            self.shiftx2_dir = None
+
+        self.shiftx2_dir = self.comm.bcast(self.shiftx2_dir, root=0)
+
+    def get_fname(self, dir, s):
+        return os.path.join(dir, s)
+
+    def get_files(self, dir, pattern):
+        
+        files = fnmatch.filter(os.listdir(dir), pattern)
+        #files = [os.path.abspath(self.getFname(dir, sn)) for sn in files]
+        files = [self.get_fname(dir, sn) for sn in files]
+        RE_DIGIT = re.compile(r'(\d+)')
+        ALPHANUM_KEY = lambda s: [int(g) if g.isdigit() else g for g in RE_DIGIT.split(s)]
+        files.sort(key = ALPHANUM_KEY)
+        
+        return files
+
+    def get_range(self, length, interval):
+
+        # Si process 0
+        if self.rank == 0:
+            # On genere une liste de la taille de la simulation
+            N = np.arange(start=0, stop=length, step=interval, dtype=np.int32)
+            # On la coupe en part egale
+            N_part = np.array_split(N, self.size)
+
+            # On supprime la liste N, car elle ne sert plus a rien
+            del(N)
+
+            # Si on a plus de 1 processus
+            if self.size > 1:
+                # On envoie un message a chaque process
+                for part, dest in zip(N_part[1:], np.arange(1, self.size)):
+                    self.comm.send([part[0], part[-1]], dest=dest)
+
+            # Puis on retourne le range du process 0
+            tmp = [N_part[0][0], N_part[0][-1]]
+            return tmp[0], tmp[1]
+
+        # Si process non 0
+        elif self.rank > 0:
+            # On retourne le message recu du process 0
+            tmp = self.comm.recv(source=0)
+            return tmp[0], tmp[1]
+
+    def run_shiftx(self, directory, pH=7.4, temperature=298.15):
+        cmd = 'java -cp %s/bin:%s/lib/weka.jar ShiftXp -b \'%s/*.pdb\' -atoms ALL -ph %s -temp %s -dir %s' 
+        cmd = cmd % (self.shiftx2_dir, self.shiftx2_dir, directory, pH, temperature, self.shiftx2_dir)
+
+        args = shlex.split(cmd)
+
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, errors = p.communicate()
+
+        return output, errors
+
+    def read_sxp_files(self, sxp_files):
+
+        df = None
+
+        for sxp_file in sxp_files:
+            if df is None:
+                df = pd.read_csv(sxp_file)
+            else:
+                df = pd.merge(df, pd.read_csv(sxp_file), on=['NUM', 'RES', 'ATOM', 'IUPAC'], 
+                              how='outer', suffixes=['', 'x'])
+
+        df = df.sort_values(by='NUM').reset_index(drop=True)
+        df.rename(columns=lambda x: x.replace('x', ''), inplace=True)
+
+        return df
+
+    def create_dataset(self, sxp, size, shift=0, filename='shiftx.hdf5'):
+        with h5py.File(filename, 'w', driver='mpio', comm=self.comm) as f:
+            for index, row in sxp.iterrows():
+                nset = '%d_%s/%s' % (row['NUM']+shift, row['RES'], row['ATOM'])
+                dset = f.create_dataset(nset, (size,), dtype=np.float32)
+
+
+    def fill_dataset(self, sxp, start=0, shift=0, filename='shiftx.hdf5'):
+        with h5py.File(filename, 'r+', driver='mpio', comm=self.comm) as f:
+            for index, row in sxp.iterrows():
+                nset = '%d_%s/%s' % (row['NUM']+shift, row['RES'], row['ATOM'])
+
+                try:
+                    value = row['PRED_CS'].values.astype(np.float32)
+                    stop = start + value.shape[0]
+
+                except AttributeError:
+                    value = np.float32(row['PRED_CS'])
+                    stop = start + 1
+
+                try:
+                    dset = f[nset]
+                    dset[start:stop,] = value
+                except KeyError:
+                    print "Warning: unable to access to %s" % nset
+                    pass
+
+    def run(self, pH=7.4, temperature=298.15, interval=1, shift=0):
+
+        if self.pdb_dir:
+
+            # Monoprocess for pdb extraction
+            if self.rank == 0:
+
+                # Fire off shiftx2!!
+                self.run_shiftx(self.pdb_dir, pH=pH, temperature=temperature)
+                # Get list of all the cs file
+                sxp_files = self.get_files(self.pdb_dir, '*.sxp')
+                # Read all the sxp files
+                sxp = self.read_sxp_files(sxp_files)
+
+                # Create dataset and fill it directly
+                self.create_dataset(sxp, len(sxp_files), filename='shiftx.hdf5')
+                self.fill_dataset(sxp, filename='shiftx.hdf5')
+
+                # And we remove every thing!
+                [os.remove(f) for f in glob.glob('%s/*.sxp' % self.pdb_dir)]
+
+        else:
+
+            # Open trajectories!
+            u = Universe(self.top_file, self.dcd_file)
+            total_frames = len(u.trajectory)
+            used_frames = (total_frames / interval)
+
+            if self.rank == 0:
+                if not total_frames % interval == 0:
+                    print 'Error: The interval is not a multiple of the total number of frame !'
+                    self.comm.Abort()
+
+                print "Total number of frames: %s (%s frames used)" % (total_frames, used_frames)
+
+            start, stop = self.get_range(total_frames, interval)
+
+            j = 1
+            created = False
+            tmp_dir = 'tmp_shiftx'
+            tmp_rank = '%s/tmp_%s' % (tmp_dir, self.rank)
+
+            # Save the orginal path
+            ori_path = os.getcwd()
+
+            # Create directories
+            os.makedirs(tmp_rank)
+            os.chdir(tmp_rank)
+            
+            for i in xrange(start, stop+interval, interval):
+
+                # Go to the frame i
+                u.trajectory[i]
+                # And extract the PDB
+                # The method zfill() pads string on the left with zeros to fill width.
+                u.atoms.write("snapshot_%s.pdb" % str(i).zfill(len(str(total_frames))))
+
+                # We extract 5000 structures at a time
+                if (j % 5000) == 0 or stop == i:
+
+                    # Fire off shiftx2 !!
+                    self.run_shiftx('.', pH=pH, temperature=temperature)
+
+                    # Get list of all the cs file
+                    sxp_files = self.get_files('.', '*.sxp')
+                    # Read all the sxp files
+                    sxp = self.read_sxp_files(sxp_files)
+
+                    # Create dataset
+                    if not created:
+                        self.create_dataset(sxp, used_frames, shift, '%s/shiftx.hdf5' % ori_path)
+                        created = True
+
+                    # And fill it with data !
+                    numstruct = np.int(sxp_files[0].split('_')[-1].split('.')[0])
+                    start_at = np.int(numstruct / interval)
+                    self.fill_dataset(sxp, start_at, shift, '%s/shiftx.hdf5' % ori_path)
+
+                    # And we remove every thing !
+                    [os.remove(pdb_file) for pdb_file in os.listdir('.')]
+
+                    j = 1
+
+                j += 1
+
+            # And we go back in dir_output and clean
+            os.chdir(ori_path)
+
+            if self.rank == 0:
+                shutil.rmtree(tmp_dir)
+
+def parse_options():
+
+    # Define MPI environnment
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    parser = argparse.ArgumentParser(description = 'Run SHIFTX+ with MPI')
+    parser.add_argument('-p', "--pdb", dest='pdb_dir',
+                        action="store", type=str,
+                        help="directory with pdb files")
+    parser.add_argument('-t', "--top", dest='top_file',
+                        action="store", type=str,
+                        help="top file used for simulation (HS[EDP] --> HIS)")
+    parser.add_argument('-d', "--dcd", dest='dcd_file',
+                        action="store", type=str, nargs='+',
+                        help="one dcd file or a list of dcd files")
+    parser.add_argument('-i', "--interval", dest='interval',
+                        action="store", type=int, default=1,
+                        help="used frames at this interval")
+    parser.add_argument('-s', "--shift", dest='shift',
+                        action="store", type=int, default=0,
+                        help="number to shift residu number")
+    parser.add_argument('--pH', dest='pH',
+                        action="store", type=float, default=7.4,
+                        help="pH")
+    parser.add_argument('--temp', dest='temperature',
+                        action="store", type=float, default=298.15,
+                        help="temperature in Kelvin")
+
+    return parser.parse_args()
+
+def main():
+    # Parse the passing arguments
+    options = parse_options()
+
+    pdb_dir = options.pdb_dir
+    top_file = options.top_file
+    dcd_file = options.dcd_file
+    pH = options.pH
+    temperature = options.temperature
+    shift = options.shift
+    interval = options.interval
+
+    S = Shiftx(pdb_dir, top_file, dcd_file)
+    S.run(pH, temperature, interval, shift)
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
